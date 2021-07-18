d4a4b4c657546540c94d769858e93c63dfc933b1
[Andrew Core] <[adcore89@gmail.com]>
2021-07-18 12:40:10 -0400

    user model created

--- models/User.js
+++ models/User.js
@@ -1,8 +1,64 @@
 const {Model, Datatypes } = require('sequelize');
+const bcrypt = require('bcrypt');
 const sequlize = require('../config/connection');
 
-class User extends Model {}
+class User extends Model {
 
-User.init()
+checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw,this.password);
+}
+}
 
-module.export = User
+
+
+User.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true
+        }
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [4]
+        }
+      }
+    },
+    {
+      hooks: {
+        // set up beforeCreate lifecycle "hook" functionality
+        async beforeCreate(newUserData) {
+          newUserData.password = await bcrypt.hash(newUserData.password, 10);
+          return newUserData;
+        },
+  
+        async beforeUpdate(updatedUserData) {
+          updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+          return updatedUserData;
+        }
+      },
+      sequelize,
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'user'
+    }
+  );
+  
+  module.exports = User;
+  
